675b4e69c7b10a549193924bd650b9747b50d411
galib <ultansms@gmail.com>
2022-09-16 10:14:07 +0600

    basic endpoints done so far

---
+++ readme.md
@@ -0,0 +1,13 @@
+# capstone-quizapp-backend-repo
+
+### day-1
+
+## 15-09-2022
+
+9:00 --- 5:00 worked on singin, signup, (backend and frontend)
+
+### day-2
+
+## 16-09-2022
+
+9:30--10:30 worked on quiz, question endpoints
--- route/quiz_route.js
+++ route/quiz_route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const QuizModel = require("../models/quizModel");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { title, topic } = req.body;
 
   if (!title || !topic) {
@@ -12,9 +12,60 @@ router.post("/", (req, res) => {
       title,
       topic,
     });
+
+    try {
+      const saveNewQuiz = await setNewQuiz.save();
+
+      return res
+        .status(200)
+        .send(
+          `New quiz has been created with id ${saveNewQuiz._id},${title} and ${topic}`
+        );
+    } catch (error) {
+      res.status(500).send(`ERROR ${error}`);
+    }
   }
 
   return res.status(200).send("quiz endpoint reached");
+});
+
+router.get("/quizDetails/:quizId", async (req, res) => {
+  const { quizId } = req.params;
+
+  if (quizId === undefined) {
+    return res.status(400).send("A valid quiz ID is required");
+  } else {
+    try {
+      const getQuizById = await QuizModel.findById(quizId);
+
+      if (getQuizById === null) {
+        return res.status(400).send("No quiz is found with this id");
+      }
+      return res.status(200).json(getQuizById);
+    } catch (error) {
+      res.status(500).send(`ERROR ${error}`);
+    }
+  }
+});
+
+router.delete("/delete/:quizId", async (req, res) => {
+  const { quizId } = req.params;
+
+  if (quizId === undefined) {
+    return res.status(400).send("A valid quiz ID is required");
+  } else {
+    try {
+      const getQuizById = await QuizModel.findById(quizId);
+
+      if (getQuizById === null) {
+        return res.status(400).send("No quiz is found with this id");
+      }
+      const deletedQuiz = await QuizModel.findByIdAndDelete(quizId);
+      return res.status(200).send(`A quiz has been deleted with id ${quizId}`);
+    } catch (error) {
+      res.status(500).send(`ERROR ${error}`);
+    }
+  }
 });
 
 module.exports = router;
